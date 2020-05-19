0a36c7f31a2480c4a4fcea31c17a06e5eaa1582e
sheshantsinha <b216040@iiit-bh.ac.in>
2020-05-19 17:35:02 +0530

    merged all pr in one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,4 @@ export const environment = {
   callback_uri: $ENV.CALLBACK_URI,
   api_url: $ENV.API_URL,
   app_url: $ENV.APP_URL,
-  qbo_client_id: $ENV.QBO_CLIENT_ID,
-  qbo_scope: $ENV.QBO_SCOPE,
-  qbo_authorize_uri: $ENV.QBO_AUTHORIZE_URI,
-  qbo_app_url: $ENV.QBO_APP_URL
 };
